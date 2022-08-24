af7bc4d319984002c02883074b3da617a50b10c1
smohan67 <siddhu.mohan6@gmail.com>
2022-08-23 23:36:34 -0500

    add server on example client

--- ServerApiClient-Example/ClientCode/ServerApiClient.cs
+++ ServerApiClient-Example/ClientCode/ServerApiClient.cs
@@ -6,10 +6,21 @@ namespace ServerApiClient_Example.ClientCode
 {
     public class ServerApiClient : MonoBehaviour
     {
+        public string BaseUrl;
+        public uint Port;
         public IEnumerator AddServer(string serverName, string serverip, uint maxPlayers, uint currentNumPlayers,
             bool isPrivate)
         {
-            yield return null;
+            using (UnityWebRequest www = new UnityWebRequest($"{BaseUrl}{Port}", "POST"))
+            {
+                www.SetRequestHeader(Constants.ServerName, serverName);
+                www.SetRequestHeader(Constants.ServerIp, serverip);
+                www.SetRequestHeader(Constants.MaxPlayers, maxPlayers.ToString());
+                www.SetRequestHeader(Constants.CurrentNumberOfPlayers, currentNumPlayers.ToString());
+                www.SetRequestHeader(Constants.IsPrivate, isPrivate.ToString());
+                yield return www.SendWebRequest();
+                Debug.Log($"{www.downloadHandler.text}");
+            }
         }
     }
 }
